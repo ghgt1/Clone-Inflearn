1831f516002c253c4f2d8b54051ac8a0c1d57a3f
JYROH <ghgt1@naver.com>
2022-10-04 19:53:18 +0900

    feat: Create Swiper banner

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
 "use strict";
 
-new Swiper(".banner .swiper-container", {
+var mySwiper = new Swiper(".banner .swiper-container", {
   loop: true,
-  //   autoplay: {
-  //     delay: 5000,
-  //   },
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: ".banner .swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    prevEl: ".banner .swiper-prev",
+    nextEl: ".banner .swiper-next",
+  },
+});
+var btn_start = document.querySelector(".swiper-start");
+var btn_pause = document.querySelector(".swiper-pause");
+
+btn_start.addEventListener("click", function () {
+  mySwiper.autoplay.start();
+});
+btn_pause.addEventListener("click", function () {
+  mySwiper.autoplay.stop();
 });
